76852eeaba3302f88da25ef79421c33d8b8a9b6e
mpr015 <przybylski.mj@gmail.com>
2020-11-12 20:21:02 +0100

    initial commit

---
+++ myThermalConductionSolver.C
@@ -0,0 +1,70 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2020 2020 AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    myThermalConductionSolver
+
+Description
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+
+    #include "createMesh.H"
+    #include "createFields.H"
+
+    //k is missing and it could be hardcoded, like below, but it is better to
+    //be able to chane it and read it at runtime - look for def in createFields.H
+    /*
+    dimensionedScalar k
+    (
+      "k",
+      dimensionSet(0, 2, -1, 0, 0, 0, 0),
+      scalar(4e-05)
+    );
+    */
+
+    solve(fvm::laplacian(k, T) + su + fvm::Sp(sp, T));
+    runTime++;
+    runTime.write();
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< nl;
+    runTime.printExecutionTime(Info);
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
